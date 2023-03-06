b5380c223fb9e516a439577128b2215f009167d9
yuki-o1o5 <jkksst1712@gmail.com>
2023-03-06 14:44:10 -0800

    docs/change the error msg

--- src/pageComponents/SignUp/SignUp.jsx
+++ src/pageComponents/SignUp/SignUp.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
+import { createTheme } from "@mui/material/styles";
 import "./SignUp.css";
 
 function SignUp() {
-  const initialValues = { username: "", mailAddress: "", passWord: "" };
-  const [formValues, setFormValues] = useState(initialValues);
+  const initialUser = { username: "", mailAddress: "", passWord: "" };
+  const [formValues, setFormValues] = useState(initialUser);
   const [formErrors, setFormErrors] = useState({});
   const [isSubmit, setIsSubmit] = useState(false);
 
@@ -17,8 +19,23 @@ function SignUp() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setFormErrors(validate(formValues));
-    setIsSubmit(true);
+    const errors = validate(formValues);
+    setFormErrors(errors);
+    if (Object.keys(errors).length === 0) {
+      const existingData = JSON.parse(localStorage.getItem("userData")) || [];
+      const newData = {
+        username: formValues.username,
+        mailAddress: formValues.mailAddress,
+        passWord: formValues.passWord,
+        score: 0,
+      };
+      localStorage.setItem(
+        "userData",
+        JSON.stringify([...existingData, newData])
+      );
+
+      setIsSubmit(true);
+    }
   };
 
   const validate = (values) => {
@@ -26,82 +43,100 @@ function SignUp() {
     const regex =
       /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (!values.username) {
-      errors.username = "Can you write user name?";
+      errors.username = "User name is required.";
     }
     if (!values.mailAddress) {
-      errors.mailAddress = "Can you write e-mail adress?";
+      errors.mailAddress = "E-mail address is required.";
     } else if (!regex.test(values.mailAddress)) {
-      errors.mailAddress = "Please write collect e-mail adress";
+      errors.mailAddress = "Please write correct e-mail adress.";
     }
     if (!values.passWord) {
-      errors.passWord = "Can you write password?";
+      errors.passWord = "Password is required.";
     } else if (values.passWord.length < 4) {
-      errors.passWord =
-        "Can you write more than 4 letters & less than 15 letters?";
+      errors.passWord = "4 to 15 characters, without spaces.";
     } else if (values.passWord.length > 15) {
-      errors.passWord =
-        "Can you write more than 4 letters & less than 15 letters?";
+      errors.passWord = "4 to 15 characters, without spaces.";
     }
 
     return errors;
   };
 
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#39707a",
+      },
+    },
+  });
+
   return (
     <div className="formCotainer">
-      <form onSubmit={(event) => handleSubmit(event)}>
+      <form className="formControl" onSubmit={(event) => handleSubmit(event)}>
         <h1>Sign Up</h1>
         <div className="accountLink">
           <p className="signUpLink">Already have an account? </p>
-          <a href="#">
-            <span>Log in</span>
-          </a>
+          <Link to="/">
+            <span className="link-to-login">Log in</span>
+          </Link>
         </div>
         <hr />
         <div className="uiForm">
-          <div className="formFeild">
-            <label>User Name</label>
-            <input
-              type="text"
-              placeholder="user name"
-              name="username"
-              onChange={(event) => handleChange(event)}
-            />
+          <div className="formFeildControler">
+            <div className="formFeild">
+              {/* <label>User Name</label> */}
+              <input
+                type="text"
+                placeholder="User name"
+                name="username"
+                onChange={(event) => handleChange(event)}
+              />
+            </div>
+            <p className="errorMsg">{formErrors.username}</p>
           </div>
-          <p className="errorMsg">{formErrors.username}</p>
-
-          <div className="formFeild">
-            <label>E-mail adress</label>
-            <input
-              type="text"
-              placeholder="e-mail adress"
-              name="mailAddress"
-              onChange={(event) => handleChange(event)}
-            />
+          <div className="formFeildControler">
+            <div className="formFeild">
+              {/* <label>E-mail adress</label> */}
+              <input
+                type="text"
+                placeholder="E-mail address"
+                name="mailAddress"
+                onChange={(event) => handleChange(event)}
+              />
+            </div>
+            <p className="errorMsg">{formErrors.mailAddress}</p>
           </div>
-          <p className="errorMsg">{formErrors.mailAddress}</p>
-
-          <div className="formFeild">
-            <label>Password</label>
-            <input
-              type="text"
-              placeholder="password"
-              name="passWord"
-              onChange={(event) => handleChange(event)}
-            />
+          <div className="formFeildControler">
+            <div className="formFeild">
+              {/* <label>Password</label> */}
+              <input
+                type="text"
+                placeholder="Password"
+                name="passWord"
+                onChange={(event) => handleChange(event)}
+              />
+            </div>
+            <p className="errorMsg">{formErrors.passWord}</p>
           </div>
 
-          <p className="errorMsg">{formErrors.passWord}</p>
           <Button
+            theme={theme}
             variant="contained"
             className="submitButton"
-            sx={{ mt: 5, p: 1 }}
+            sx={{ p: 1.5 }}
             type="submit"
             currsol="point"
+            fullWidth
+            style={{ borderRadius: 10 }}
           >
             Sign up
           </Button>
           {Object.keys(formErrors).length === 0 && isSubmit && (
-            <div className="msgOK">Sucess!</div>
+            <div className="msgOK">
+              Sucess!{" "}
+              <Link to="/">
+                <span className="link-to-login"> Log in</span>
+              </Link>
+            </div>
           )}
         </div>
       </form>
