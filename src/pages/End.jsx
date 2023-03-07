510e6f87fee830c3d099c9e8b6b21af7c1578dc2
yuki-o1o5 <jkksst1712@gmail.com>
2023-03-07 14:32:36 -0800

    fix/change className

--- src/pages/End.jsx
+++ src/pages/End.jsx
@@ -17,13 +17,13 @@ function End() {
   let sortedUsers = existingData.sort(function compareNumbers(a, b) {
     return b.score - a.score;
   });
-  
+
   topPlayers = sortedUsers.slice(0, 3);
   console.log(topPlayers);
 
   return (
     <div>
-      <div class="confetti">
+      <div className="confetti">
         <span></span>
         <span></span>
         <span></span>
@@ -77,9 +77,9 @@ function End() {
       <Score
         myScore={currentScore}
         allQ="20"
-        userName1={topPlayers[0].username }
-        userName2={topPlayers[1].username }
-        userName3={topPlayers[2].username }
+        userName1={topPlayers[0].username}
+        userName2={topPlayers[1].username}
+        userName3={topPlayers[2].username}
       />
     </div>
   );
