ff6dae7c931501548de7aa3ee3a0396fbca340a0
yuki-o1o5 <jkksst1712@gmail.com>
2023-03-07 14:29:08 -0800

    docs/add button

--- src/pageComponents/Score/Score.jsx
+++ src/pageComponents/Score/Score.jsx
@@ -1,7 +1,32 @@
-import StepButtonSm from "../../commonComponents/StepButtonSm/StepButtonSm.jsx";
+import Button from "@mui/material/Button";
+import { createTheme } from "@mui/material/styles";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Score.css";
 
 function Score({ myScore, allQ, userName1, userName2, userName3 }) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, []);
+
+  const goToCategoriesPage = () => {
+    navigate("/start");
+  };
+
+  const goToLoginPage = () => {
+    navigate("/");
+  };
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#39707a",
+      },
+    },
+  });
+
   return (
     <div className="primaryContainer">
       <div className="scoreCotainer">
@@ -9,34 +34,56 @@ function Score({ myScore, allQ, userName1, userName2, userName3 }) {
           <h2>Your Score</h2>
           <div className="score">
             {myScore}
-            <span>/{allQ}</span>
+            <span> / {allQ}</span>
           </div>
         </div>
         <div className="topPlayers">
-          <h2>Top 3 players</h2>
-          <div className="topPlayersList">
-            <div className="orderContainer">
-              <div className="bar2">2</div>
-              <div className="orderName">
-                <div>{userName2}</div>
+          <div className="barsContainer">
+            <h2>Top 3 players</h2>
+            <div className="topPlayersList">
+              <div className="orderContainer">
+                <div className="bar2">2</div>
+              </div>
+              <div className="orderContainer">
+                <div className="bar1">1</div>
+              </div>
+              <div className="orderContainer">
+                <div className="bar3">3</div>
               </div>
             </div>
-            <div className="orderContainer">
-              <div className="bar1">1</div>
-              <div className="orderName">
-                <div>{userName1}</div>
-              </div>
-            </div>
-            <div className="orderContainer">
-              <div className="bar3">3</div>
-              <div className="orderName">
-                <div>{userName3}</div>
-              </div>
-            </div>
+            <hr className="underBar" />
+          </div>
+          <div className="topPlayersNameContainer">
+            <div className="username">{userName2}</div>
+            <div className="username">{userName1}</div>
+            <div className="username">{userName3}</div>
           </div>
         </div>
         <div className="stepButtonContainer">
-          <StepButtonSm word="Try Again" />
+          <Button
+            theme={theme}
+            variant="contained"
+            className="submitButton"
+            sx={{ p: 1.5 }}
+            type="submit"
+            currsol="point"
+            style={{ borderRadius: 10 }}
+            onClick={goToCategoriesPage}
+          >
+            Retry
+          </Button>
+          <Button
+            theme={theme}
+            variant="contained"
+            className="submitButton"
+            sx={{ p: 1.5 }}
+            type="submit"
+            currsol="point"
+            style={{ borderRadius: 10 }}
+            onClick={goToLoginPage}
+          >
+            Log out
+          </Button>
         </div>
       </div>
     </div>
